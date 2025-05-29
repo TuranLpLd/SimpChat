2ed456de37a9296a411c2ee263c10e99d3deab53
TuranLpLd <485235662@qq.com>
2025-05-29 19:51:24 +0800

    创建项目

---
+++ SimpChatBackend/src/main/java/top/turan/simpchatbackend/SimpChatBackendApplication.java
@@ -0,0 +1,13 @@
+package top.turan.simpchatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpChatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpChatBackendApplication.class, args);
+    }
+
+}
---
+++ SimpChatBackend/src/test/java/top/turan/simpchatbackend/SimpChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package top.turan.simpchatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpChatBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
